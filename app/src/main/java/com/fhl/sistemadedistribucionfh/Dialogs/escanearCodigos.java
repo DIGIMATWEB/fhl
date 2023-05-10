4c6420f0d2b97c11ffc0353e64e22be26625dfb0
Heladio Francisco Morales Dorantes <inghfrancisco.morales@gmail.com>
2023-05-10 12:56:41 -0600

    dialogo de escaneo de codigos

---
+++ app/src/main/java/com/fhl/sistemadedistribucionfh/Dialogs/escanearCodigos.java
@@ -0,0 +1,55 @@
+package com.fhl.sistemadedistribucionfh.Dialogs;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.fhl.sistemadedistribucionfh.R;
+
+public class escanearCodigos extends DialogFragment implements View.OnClickListener {
+    public static final String TAG = escanearCodigos.class.getSimpleName();
+    private Button iralmenu;
+    private TextView titleheader;
+    private ImageView imagebackground;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NO_TITLE, android.R.style.Theme_DeviceDefault_Light_NoActionBar);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_escancodes, container, false);
+        getDialog().getWindow().setBackgroundDrawableResource(R.color.customTransparent);
+        setCancelable(true);
+        initDialog(view);
+        //setFonts();
+        return view;
+    }
+
+    private void initDialog(View view) {
+
+    }
+
+    public void closeDialog() {
+        this.dismiss();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+        }
+    }
+}
