01b53cd7e0325d31f50f8403d3b52df1bd3fd582
DIGIMAT <36770624+DIGIMATWEB@users.noreply.github.com>
2024-02-20 13:23:04 -0600

    save images with index of kind of image

--- app/src/main/java/com/fhl/sistemadedistribucionfh/evidence/photos/carrusel.java
+++ app/src/main/java/com/fhl/sistemadedistribucionfh/evidence/photos/carrusel.java
@@ -10,7 +10,6 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
 import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Log;
@@ -35,25 +34,21 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
-import java.util.Set;
 
 public class carrusel extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
         public static final String TAG = carrusel.class.getSimpleName();
         private Button guardarFotosButon;
         private ImageView backImage;
-        private ImageButton imageView19, imageButton3, imageButton4, imageButton5, imageButton6, imageButton7, imageButton8, imageButton9;
+        private ImageButton imageButton1, imageButton3, imageButton4, imageButton5, imageButton6, imageButton7, imageButton8, imageButton9;
         private static final int REQUEST_IMAGE_CAPTURE = 1;
         private ImageButton lastClickedImageButton; // Keep track of the last clicked ImageButton
         private File tempImageFile;
         private ArrayList<File> tempImageFiles = new ArrayList<>();
         private List<String> directories=new ArrayList<>();
         private ImageButton sawLast,eraseFolder;
+        private Integer lasphoto=0;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
@@ -64,7 +59,7 @@ public class carrusel extends AppCompatActivity implements View.OnClickListener,
 
         private void initView() {
                 backImage = findViewById(R.id.backImage);
-                imageView19 = findViewById(R.id.imageView19);
+                imageButton1 = findViewById(R.id.imageButton1);
                 imageButton3 = findViewById(R.id.imageButton3);
                 imageButton4 = findViewById(R.id.imageButton4);
                 imageButton5 = findViewById(R.id.imageButton5);
@@ -74,8 +69,8 @@ public class carrusel extends AppCompatActivity implements View.OnClickListener,
                 imageButton9 = findViewById(R.id.imageButton9);
 
                 backImage.setOnClickListener(this);
-                imageView19.setOnClickListener(this);
-                imageView19.setOnLongClickListener(this);
+                imageButton1.setOnClickListener(this);
+                imageButton1.setOnLongClickListener(this);
                 imageButton3.setOnClickListener(this);
                 imageButton3.setOnLongClickListener(this);
                 imageButton4.setOnClickListener(this);
@@ -99,6 +94,14 @@ public class carrusel extends AppCompatActivity implements View.OnClickListener,
 
                 guardarFotosButon = findViewById(R.id.guardarFotosButon);
                 guardarFotosButon.setOnClickListener(this);
+                imageButton1.setTag(1);
+                imageButton3.setTag(3);
+                imageButton4.setTag(4);
+                imageButton5.setTag(5);
+                imageButton6.setTag(6);
+                imageButton7.setTag(7);
+                imageButton8.setTag(8);
+                imageButton9.setTag(9);
         }
 
         @Override
@@ -111,16 +114,16 @@ public class carrusel extends AppCompatActivity implements View.OnClickListener,
                                 lastClickedImageButton.setImageBitmap(imageBitmap);
                                 // Save the image to a temporary file
                               //  tempImageFile = saveTempImage(imageBitmap);
-                                tempImageFiles.add(saveTempImage(imageBitmap));
+                                tempImageFiles.add(saveTempImage(imageBitmap,lasphoto));
                         }
                 }
         }
 
-        private File saveTempImage(Bitmap bitmap) {
+        private File saveTempImage(Bitmap bitmap, Integer lasphoto) {
                 File tempDir = getCacheDir();
                 File tempFile = null;
                 try {
-                        tempFile = File.createTempFile("temp_image", ".jpg", tempDir);
+                        tempFile = File.createTempFile(String.valueOf(lasphoto)+"_"+ "temp_image", ".jpg", tempDir);
                         FileOutputStream out = new FileOutputStream(tempFile);
                         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
                         out.flush();
@@ -158,7 +161,8 @@ public class carrusel extends AppCompatActivity implements View.OnClickListener,
                 dialog.show();
         }
 
-        private void openCamera() {
+        private void openCamera(int index) {
+                this.lasphoto=index;
                 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
                         startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
@@ -332,7 +336,7 @@ public class carrusel extends AppCompatActivity implements View.OnClickListener,
                         case R.id.backImage:
                                 onBackPressed();
                                 break;
-                        case R.id.imageView19:
+                        case R.id.imageButton1:
                         case R.id.imageButton3:
                         case R.id.imageButton4:
                         case R.id.imageButton5:
@@ -343,7 +347,7 @@ public class carrusel extends AppCompatActivity implements View.OnClickListener,
                                 // Set the lastClickedImageButton to the currently clicked ImageButton
                                 lastClickedImageButton = (ImageButton) v;
                                 // Open camera to capture image
-                                openCamera();
+                                openCamera((int) v.getTag());
                                 break;
                 }
         }
@@ -352,8 +356,8 @@ public class carrusel extends AppCompatActivity implements View.OnClickListener,
         public boolean onLongClick(View v) {
                 Drawable drawable = null;
                 switch (v.getId()) {
-                        case R.id.imageView19:
-                                drawable = imageView19.getDrawable();
+                        case R.id.imageButton1:
+                                drawable = imageButton1.getDrawable();
                                 break;
                         case R.id.imageButton3:
                                 drawable = imageButton3.getDrawable();
