b39f46a21584091b58042ec66c106cce60261cff
DIGIMAT <36770624+DIGIMATWEB@users.noreply.github.com>
2024-03-14 18:51:53 -0600

    pending model for sendchecklist

---
+++ app/src/main/java/com/fhl/sistemadedistribucionfh/checkList/Questions/model/sendChecklist/sendChecklist.java
@@ -0,0 +1,5 @@
+package com.fhl.sistemadedistribucionfh.checkList.Questions.model.sendChecklist;
+
+public class sendChecklist {//todo este debe ser un objeto para guardar las respuestas previamente seleccionadas en el checklist antes del sendchecklist
+
+}
--- app/src/main/java/com/fhl/sistemadedistribucionfh/checkList/Questions/view/QuestionFragment2.java
+++ app/src/main/java/com/fhl/sistemadedistribucionfh/checkList/Questions/view/QuestionFragment2.java
@@ -15,7 +15,9 @@ import androidx.fragment.app.Fragment;
 import com.fhl.sistemadedistribucionfh.R;
 import com.fhl.sistemadedistribucionfh.checkList.Questions.model.Question;
 import com.fhl.sistemadedistribucionfh.checkList.model.v2.Pregunta;
+import com.fhl.sistemadedistribucionfh.checkList.model.v2.Respuesta;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class QuestionFragment2 extends Fragment {
@@ -24,7 +26,8 @@ public class QuestionFragment2 extends Fragment {
     private  questionFragment mview;
     private ConstraintLayout switchanswer,optionanswer,openanswer;
     private List<Pregunta> mquestions;
-    private TextView textopenquestion,textopenquestion1,textbooleanonly;
+    private TextView textopenquestion,textopenquestion1,textbooleanonly,textPosition1,textPosition2,textPosition3;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -39,23 +42,37 @@ public class QuestionFragment2 extends Fragment {
         openanswer=view.findViewById(R.id.openanswer);
         textopenquestion =view.findViewById(R.id.textopenquestion);
         textopenquestion1=view.findViewById(R.id.textopenquestion1);
-        textbooleanonly=view.findViewById(R.id. textbooleanonly);
+        textbooleanonly=view.findViewById(R.id.textbooleanonly);
+        textPosition1=view.findViewById(R.id.textPosition);
+        textPosition2=view.findViewById(R.id. textPosition2);
+        textPosition3=view.findViewById(R.id.textPosition3);
+        // Set textPosition after initialization
+
+
         if(question.getTipoCampo()==1){
+            textPosition1.setText((pos + 1) + "/" + mquestions.size());
+            textPosition1=view.findViewById(R.id.textPosition);
             switchanswer.setVisibility(View.VISIBLE);
             openanswer.setVisibility(View.GONE);
             optionanswer.setVisibility(View.GONE);
-        }else if(question.getTipoCampo()==2){
+        } else if(question.getTipoCampo()==2){
+            textPosition2.setText((pos + 1) + "/" + mquestions.size());
             openanswer.setVisibility(View.VISIBLE);
             switchanswer.setVisibility(View.GONE);
             optionanswer.setVisibility(View.GONE);
-
-        }else if(question.getTipoCampo()==3){
+        } else if(question.getTipoCampo()==3){
+            textPosition3.setText((pos + 1) + "/" + mquestions.size());
             openanswer.setVisibility(View.GONE);
             switchanswer.setVisibility(View.GONE);
             optionanswer.setVisibility(View.VISIBLE);
 
             Spinner spinner = view.findViewById(R.id.spinnerquestionary);
-            String[] items = {"Item 1", "Item 2", "Item 3"}; // Replace with your desired data items
+
+            List<String> items = new ArrayList<>(); // Replace with your desired data items
+            items.clear();
+            for(Respuesta r:mquestions.get(pos).getRespuestas()){
+              items.add(r.getNombre());
+            }
             ArrayAdapter<String> adapter = new ArrayAdapter<>(this.getActivity(), android.R.layout.simple_spinner_item, items);
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             spinner.setAdapter(adapter);
@@ -63,13 +80,13 @@ public class QuestionFragment2 extends Fragment {
             spinner.setPopupBackgroundResource(android.R.color.transparent); // Make dropdown background transparent
             spinner.setDropDownVerticalOffset(spinner.getHeight()); // Set vertical offset to position the dialog
         }
+
         if (textopenquestion != null) {
             textopenquestion.setText(""+mquestions.get(pos).getNombre());
             textopenquestion1.setText(""+mquestions.get(pos).getNombre());
             textbooleanonly.setText(""+mquestions.get(pos).getNombre());
         }
     }
-
 
     public QuestionFragment2(Pregunta question, questionFragment mview, int position, List<Pregunta> questions) {
         this.question = question;
@@ -80,6 +97,5 @@ public class QuestionFragment2 extends Fragment {
 
     public static QuestionFragment2 newInstance(Pregunta question, questionFragment mview, int position, List<Pregunta> questions) {
         return new QuestionFragment2(question,mview,position,questions);
-
     }
 }
