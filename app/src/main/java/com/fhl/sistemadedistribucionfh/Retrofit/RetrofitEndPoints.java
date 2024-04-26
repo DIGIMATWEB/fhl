6396b3f5bbfdda4c1732ed506122b46a69b606b3
Angel Prado <luanpr0294@gmail.com>
2024-04-26 09:44:40 -0600

    Endpoints fixed

--- app/src/main/java/com/fhl/sistemadedistribucionfh/Retrofit/RetrofitEndPoints.java
+++ app/src/main/java/com/fhl/sistemadedistribucionfh/Retrofit/RetrofitEndPoints.java
@@ -2,15 +2,10 @@ package com.fhl.sistemadedistribucionfh.Retrofit;
 
 public class RetrofitEndPoints {
 
-<<<<<<< HEAD
     public static final String URL_FH ="https://sistemasservice.trafficmanager.net/"; 
     public static final String URL_PRODMANFEST ="https://w5r7bljm-6220.usw3.devtunnels.ms/App/Manifiesto/";//"http://192.168.1.208:7420/App/Manifiesto/";//jose//"https://operacionsgdservice.trafficmanager.net/App/Manifiesto/";//  //http://192.168.1.227:7420/
     public static final String URL_PRODVEHICLES ="https://w5r7bljm-6220.usw3.devtunnels.ms/App/Vehiculo/";//"http://192.168.1.208:7420/App/Vehiculo/";//jose//"https://operacionsgdservice.trafficmanager.net/App/Vehiculo/";// //http://192.168.1.227:7420/
-=======
-    public static final String URL_FH = "https://sistemasservice.trafficmanager.net/";
-    public static final String URL_PRODMANFEST ="http://192.168.1.225:7420/App/Manifiesto/";//"https://operacionsgdservice.trafficmanager.net/App/Manifiesto/";//  //http://192.168.1.227:7420/
-    public static final String URL_PRODVEHICLES ="http://192.168.1.225:7420/App/Vehiculo/";//"https://operacionsgdservice.trafficmanager.net/App/Vehiculo/";// //http://192.168.1.227:7420/
->>>>>>> feature_luis2
+
     //ENDPOINTS FH
     public static final String URL_FHL_SGD="https://64vdvmnq-7220.usw3.devtunnels.ms/";//"https://administracionsgdservice.trafficmanager.net/";
     public static final String URL_AVOCADO="http://35.194.83.10/WS-phoenixApiPROD/";
