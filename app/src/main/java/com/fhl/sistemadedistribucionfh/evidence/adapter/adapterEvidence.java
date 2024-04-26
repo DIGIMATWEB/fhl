c0e587d7f084aa8149d0c22b7825886d528e2bd7
DIGIMAT <36770624+DIGIMATWEB@users.noreply.github.com>
2024-04-25 19:47:00 -0600

    manage diferent tipes of evidences

--- app/src/main/java/com/fhl/sistemadedistribucionfh/evidence/adapter/adapterEvidence.java
+++ app/src/main/java/com/fhl/sistemadedistribucionfh/evidence/adapter/adapterEvidence.java
@@ -2,13 +2,16 @@ package com.fhl.sistemadedistribucionfh.evidence.adapter;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.fhl.sistemadedistribucionfh.R;
@@ -37,7 +40,33 @@ public class adapterEvidence extends RecyclerView.Adapter<adapterEvidence.ViewHo
 
     @Override
     public void onBindViewHolder(@NonNull adapterEvidence.ViewHolder holder, @SuppressLint("RecyclerView") final int position) {
-
+        switch (position) {
+            case 0://Firma siempre va
+                // Handle logic for position 0
+                Drawable background = ContextCompat.getDrawable(context, R.drawable.ic_signature_ico);
+                holder.image.setBackground(background);
+                holder.description.setText("Firma");
+                break;
+            case 1:
+                // Handle logic for position 1
+                break;
+            case 2:
+                // Handle logic for position 1
+                break;
+            case 3:
+                // Handle logic for position 1
+                break;
+            case 4:
+                // Handle logic for position 1
+                Drawable background4 = ContextCompat.getDrawable(context, R.drawable.ic_rankico);
+                holder.image.setBackground(background4);
+                holder.description.setText("Encuesta");
+                break;
+            // Add more cases as needed
+            default:
+                // Handle default logic for other positions
+                break;
+        }
 
 
     }
@@ -48,10 +77,12 @@ public class adapterEvidence extends RecyclerView.Adapter<adapterEvidence.ViewHo
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-
+        ImageView image;
+        TextView   description;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-
+            image=itemView.findViewById(R.id.imageMenu3video);
+            description=itemView.findViewById(R.id.menuName3video);
         }
     }
 }
